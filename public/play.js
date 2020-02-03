7d88e1dfbf4e487620b1f06c6366a92de9e50e1f
Isabella Possaghi <isabella.possaghi@gmail.polimi.it>
2020-02-03 14:36:21 +0100

    prova

--- public/play.js
+++ public/play.js
@@ -1,46 +1,29 @@
 //PLAY
 
-//variables
+
+
+
+function preload(){
+  // put preload code here
+}
+
 var socket;
-var playersNumber = 0;
 
 function setup() {
 
 
-    // Create a new connection using socket.io (imported in index.html)
-    socket = io();
-    // Define which function should be called when a new message
-    // comes from the server with type "mouseBroadcast"
-    socket.on('mouseBroadcast', newDrawing)
 
 
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0, 0);
-  canvas.style("z-index", "-1");
-
-  background(139, 215, 232);
-
-
-
-  // var url_string = window.location.href;
-  // var myUrl = new URL(url_string);
-  // var array_string = myUrl.searchParams.get("array");
-  // var myArray = JSON.parse(array_string);
-}
-
-
-function draw(){
-  background(139, 215, 232);
-  textAlign(CENTER,CENTER);
-  textFont('courier');
-  textSize(40);
-  fill(65,91,126);
-  text(playersNumber + "/20",windowWidth/2 ,windowHeight/2);
+  // put setup code here
+  createCanvas(800,800);
+  background('red');
+  // Create a new connection using socket.io (imported in index.html)
+  socket = io();
+  // Define which function should be called when a new message
+  // comes from the server with type "mouseBroadcast"
+  socket.on('mouseBroadcast', newDrawing)
 
 }
-
-
-
 // Callback function called when a new message comes from the server
 // Data parameters will contain the received data
 function newDrawing(data){
@@ -66,16 +49,4 @@ function mouseDragged() {
 	socket.emit('mouse', data)
 
 	ellipse(mouseX, mouseY, 20)
-}
-
-
-
-//fixed screen when you touch it
-function touchMoved() {
-  return false;
-}
-
-//function to resize the window
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
 }

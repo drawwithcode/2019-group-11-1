965218b894f287c81912c593ad37c7ac1a8b2ea2
Isabella Possaghi <isabella.possaghi@gmail.polimi.it>
2020-02-03 14:30:31 +0100

    prova

--- public/play.js
+++ public/play.js
@@ -1,21 +1,24 @@
 //PLAY
 
 //variables
+var socket;
 var playersNumber = 0;
 
 function setup() {
+
+
+    // Create a new connection using socket.io (imported in index.html)
+    socket = io();
+    // Define which function should be called when a new message
+    // comes from the server with type "mouseBroadcast"
+    socket.on('mouseBroadcast', newDrawing)
+
+
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
   canvas.style("z-index", "-1");
 
   background(139, 215, 232);
-
-
-
-
-
-
-
 
 
 
@@ -33,11 +36,37 @@ function draw(){
   textSize(40);
   fill(65,91,126);
   text(playersNumber + "/20",windowWidth/2 ,windowHeight/2);
+
 }
 
 
 
+// Callback function called when a new message comes from the server
+// Data parameters will contain the received data
+function newDrawing(data){
+	console.log('received:', data)
+	noStroke();
+	fill('yellow');
+	ellipse(data.x, data.y, 20);
 
+}
+
+function mouseDragged() {
+	console.log('sending: ',mouseX, mouseY);
+	noStroke();
+	fill(255);
+
+	// create an object containing the mouse position
+	var data = {
+		x: mouseX,
+		y: mouseY
+	}
+	// send the object to server,
+	// tag it as "mouse" event
+	socket.emit('mouse', data)
+
+	ellipse(mouseX, mouseY, 20)
+}
 
 
 

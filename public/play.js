fb62fc6a2a48e077353497f15653252f110720bd
Isabella Possaghi <isabella.possaghi@gmail.polimi.it>
2020-02-07 20:35:43 +0100

    funziona

--- public/play.js
+++ public/play.js
@@ -134,7 +134,7 @@ function handleCounter(data) {
   document.getElementById("guests").innerHTML = data.count;
   console.log('received:', data);
   //prova per verificare il numero di persone!!
-  if (data.count = 16) {
+  if (data.count >= 16) {
     imageMode(CENTER)
     image(yellow, width / 2, height / 2, yellow.width / 5, yellow.height / 5);
   }

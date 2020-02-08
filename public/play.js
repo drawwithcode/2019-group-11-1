66b3a81c7f2b469bfeb096bf587315c46710b863
LDGee <ubtrinket@hotmail.com>
2020-02-08 18:24:20 +0100

    Update play.js

--- public/play.js
+++ public/play.js
@@ -157,7 +157,18 @@ function fillRectangle(x, y) {
   timer.id('timer');
   timer.style('display','block');
 
-
+  function timeIt() {
+    decseconds++;
+    timer.html(minutes + ":" + seconds + "." + decseconds);
+    if (decseconds >= 9) {
+      decseconds -= 9;
+      seconds++;
+    }
+    if (seconds >= 60){
+      seconds -= 60;
+      minutes++;
+    }
+  }
   myVar = setInterval(timeIt, 100);
 
 
@@ -174,19 +185,6 @@ function fillRectangle(x, y) {
 
   checkCompletition(ascisse, ordinate);
 
-}
-
-function timeIt() {
-  decseconds++;
-  timer.html(minutes + ":" + seconds + "." + decseconds);
-  if (decseconds >= 9) {
-    decseconds -= 9;
-    seconds++;
-  }
-  if (seconds >= 60){
-    seconds -= 60;
-    minutes++;
-  }
 }
 
 //-------controllo che tutti i valori siano a 1

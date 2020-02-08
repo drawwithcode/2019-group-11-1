731a02e30b8ff0c5664d73c6ba532dd055ce808c
LDGee <ubtrinket@hotmail.com>
2020-02-08 15:37:51 +0100

    proiaoooo

--- public/play.js
+++ public/play.js
@@ -50,6 +50,7 @@ var ySize;
 var clickLimit = 1;
 
 var myVar;
+var timeIts;
 
 function setup() {
   strokeWeight(4);
@@ -93,30 +94,6 @@ function setup() {
     }
   }
 
-
-  //-----TIMER
-  let minutes = 0;
-  let seconds = 0;
-  let decseconds = 0;
-  var timer = createP('0:00.0');
-  timer.id('timer');
-  timer.style('display', 'none');
-  timer.html(minutes + ":" + seconds + "." + decseconds);
-
-
-  function timeIt() {
-    decseconds++;
-    timer.html(minutes + ":" + seconds + "." + decseconds);
-    if (decseconds >= 9) {
-      decseconds -= 9;
-      seconds++;
-    }
-    if (seconds >= 60){
-      seconds -= 60;
-      minutes++;
-    }
-  }
-  myVar = setInterval(timeIt, 100);
 }
 
 
@@ -174,7 +151,29 @@ function mouseClicked() {
 
 function fillRectangle(x, y) {
 
-  timer.style('display', 'block');
+  //-----TIMER
+  let minutes = 0;
+  let seconds = 0;
+  let decseconds = 0;
+  var timer = createP('0:00.0');
+  timer.id('timer');
+  timer.html(minutes + ":" + seconds + "." + decseconds);
+
+
+  timeIts = function timeIt() {
+    decseconds++;
+    timer.html(minutes + ":" + seconds + "." + decseconds);
+    if (decseconds >= 9) {
+      decseconds -= 9;
+      seconds++;
+    }
+    if (seconds >= 60){
+      seconds -= 60;
+      minutes++;
+    }
+  }
+  myVar = setInterval(timeIt, 100);
+
 
   //coloring the rectangle
   fill(51, 73, 108, 120);
